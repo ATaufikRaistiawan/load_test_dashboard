77cb9263c8dff5b6fe128a3620c1071ffc6d32f3
SEI-I_RAIS <rais@seilink.com>
2025-07-18 14:39:47 +0700

    add new table left and right

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -1,41 +1,36 @@
-<?php namespace App\Http\Controllers;
+<?php
+
+namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\MachineData;
 
-class DashboardController extends Controller {
-    public function index() {
-        $data=MachineData::orderBy('timestamp', 'desc')->take(20)->get()->reverse();
-
-        $timestamps=$data->pluck('timestamp')->map(function ($t) {
-                return \Carbon\Carbon::parse($t)->format('H:i:s');
-            });
-        $rpm=$data->pluck('rpm');
-        $revs=$data->pluck('total_revs');
-        $load=$data->pluck('load_kn');
+class DashboardController extends Controller
+{
+    public function index()
+    {
+        $data = MachineData::orderBy('timestamp', 'desc')->take(20)->get()->reverse();
+        
+        $timestamps = $data->pluck('timestamp')->map(function ($t) {
+            return \Carbon\Carbon::parse($t)->format('H:i:s');
+        });
+        $rpm = $data->pluck('rpm');
+        $revs = $data->pluck('total_revs');
+        $load = $data->pluck('load_kn');
 
         return view('dashboard', compact('data', 'timestamps', 'rpm', 'revs', 'load'));
     }
 
-    public function getData() {
-        $data=MachineData::latest()->take(10)->get()->reverse(); // or whatever query you use
+    public function getData()
+{
+    $data = MachineData::latest()->take(10)->get()->reverse(); // or whatever query you use
 
-        return response()->json([ 'timestamps'=> $data->pluck('timestamp'),
-            'rpm'=> $data->pluck('rpm'),
-            'load'=> $data->pluck('load_kn'),
-            'rows'=> $data]);
-
-
-    }
-
-    public function getStatus() {
-        $leftMachine=MachineData::where('stage', 'left')->latest()->first();
-        $rightMachine=MachineData::where('stage', 'right')->latest()->first();
-
-        return response()->json([ 'left'=> $leftMachine->status, // running or stopped
-            'right'=> $rightMachine->status,
-            ]);
-    }
-
+    return response()->json([
+        'timestamps' => $data->pluck('timestamp'),
+        'rpm' => $data->pluck('rpm'),
+        'load' => $data->pluck('load_kn'),
+        'rows' => $data
+    ]);
+}
 
 }
---
+++ database/migrations/2025_07_18_071400_create_machine_right_data_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('machine_right_data', function (Blueprint $table) {
+            $table->id();
+            $table->timestamp('timestamp')->useCurrent();
+            $table->integer('rpm');
+            $table->float('load_kn');
+            $table->bigInteger('total_revs');
+            $table->boolean('alarm')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('machine_right_data');
+    }
+};
